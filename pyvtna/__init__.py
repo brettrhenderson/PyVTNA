bd0ec3fe47d3c21a179de7649ab10ee810680bc0
brettrhenderson <brettrhenderson25@gmail.com>
2021-11-08 22:33:12 -0800

    added recursive autosummary

--- pyvtna/__init__.py
+++ pyvtna/__init__.py
@@ -1,7 +1,16 @@
 """A python package for performing variable time normalization analysis of chemical reaction rate laws."""
 
 # Add imports here
-from .vtna import *
+from .vtna import VTNA
+# from pyvtna import align
+# from pyvtna import metrics
+# from pyvtna import notebook
+# from pyvtna import readers
+# from pyvtna import signal
+# from pyvtna import testing
+# from pyvtna import visualizers
+# from pyvtna import vtna
+
 
 # Handle versioneer
 from ._version import get_versions
